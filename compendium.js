ac9801ed075cd20c2946a884b16440bd2e018c0c
jasper sun <jaspersun2019@gmail.com>
2024-01-28 03:21:57 -0800

    changed sizing

--- compendium.js
+++ compendium.js
@@ -11,16 +11,16 @@ function hover_effect(state, caller) {
     let target =  caller == 1 ? first_button : second_button;
     let callmeister = caller == 1 ? second_button : first_button;
     if (state == true) {
-        callmeister.style.fontSize = "4vw";
+        callmeister.style.fontSize = "3vw";
         callmeister.style.color = "#6bab57";
         callmeister.style.cursor = "grab"
-        target.style.fontSize = "2vw";
+        target.style.fontSize = "1vw";
         target.style.opacity = 0.7;
     } else {
-        callmeister.style.fontSize = "3vw";
+        callmeister.style.fontSize = "2vw";
         callmeister.style.color = "#000";
         callmeister.style.cursor = "pointer"
-        target.style.fontSize = "3vw";
+        target.style.fontSize = "2vw";
         target.style.opacity = 1;
     }
 }
